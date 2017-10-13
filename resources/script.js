649552b5754bc9c0a2a34686f1bad293548a34c2
Rene-Jacques Riel <rie@qlik.com>
2017-10-13 10:22:46 -0400

    removing config

--- resources/script.js
+++ resources/script.js
@@ -1,12 +1,5 @@
 // Input your config
-var config = {
-  host: 'playground.qlik.com',
-  prefix: '/showcase/',
-  port: '443',
-  isSecure: true,
-  rejectUnauthorized: false,
-  appname: '0b0fc6d5-05ce-44d7-95aa-80d0680b3559'
-}
+var config = {}
 
 var app
 var alteredState = 'SecondState'
